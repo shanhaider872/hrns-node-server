1942b6a986ff88a607712218431ca74bc73b7677
shanhaider872 <72415274+shanhaider872@users.noreply.github.com>
2023-04-13 22:09:59 -0700

    new after merge

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ api.get("/team", async (req, res) => {
   try {
 
     const data = req.body;
-
-    const docRef = db.collection('addpost_data').doc('custom-doc-id');
+    const id = (Date.now() * 1000).toString();
+    const docRef = db.collection('addpost_verification_data').doc(id);
     await docRef.set(data);
     //************************ */
 
