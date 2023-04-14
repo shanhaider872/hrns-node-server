efa1265a7828406f359e6bded7dfcb02d9f1e18f
shanhaider872 <72415274+shanhaider872@users.noreply.github.com>
2023-04-14 22:15:54 +0500

    post to get

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ var serviceAccount = require("./hrns-6ae75-firebase-adminsdk-t1mnb-1075da2c07.js
 var serviceAccount2 = require("./hrns-verifications-firebase-adminsdk-qb7t1-54889d8dbf.json");
 
 
-// const firstApp = admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
 const firstApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 },'first');
@@ -23,7 +19,7 @@ const seconndApp = admin.initializeApp({
 api.use(bodyParser.json());
 const db2 = seconndApp.firestore();
 
-api.post("/team", async (req, res) => {
+api.get("/team", async (req, res) => {
   try {
 
     const data = req.body;
@@ -32,23 +28,8 @@ api.post("/team", async (req, res) => {
     await docRef.set(data);
     //************************ */
 
-  // Write the data to the target database here
     res.send('Data transfer complete');
 
-    // const liam2 = await db.collection('addpost_data').doc('1680059879895059').get();
-
-    // if (!liam2.exists) {
-    //   console.log('No document');
-    //  } else {
-    //   console.log(liam2.data());
-    //   res.send({ team: liam2.data() });
-    //  }
-
-    // const usersDb = db2.collection('addpost_verification_data');
-    // const liam = usersDb.doc('1680059879895059');
-    // await liam.set(liam2.data());
-
-    // console.log(users, 'users')
 
   } catch (error) {
     console.log(error, "error");
